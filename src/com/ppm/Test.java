a31250f80fa2894876e5eb7ede6e042a2608b4d0
Paing Pyae Man <paingpyaeman04@gmail.com>
2020-12-02 20:22:38 +0630

    Release CalcV1

--- src/com/ppm/Test.java
+++ src/com/ppm/Test.java
@@ -3,7 +3,12 @@ package com.ppm;
 public class Test {
 
 	public static void main(String[] args) {
-
+		Calculator cal = new Calculator();
+		int addition = cal.add(5, 3);
+		int subtraction = cal.subtract(5, 3);
+		System.out.println(addition);
+		System.out.println(subtraction);
+		
 	}
 
 }
