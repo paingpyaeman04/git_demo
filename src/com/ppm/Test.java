f30f866bd9559f5331766ee9124a3ef96f259b58
Paing Pyae Man <62030108+paingpyaeman04@users.noreply.github.com>
2020-12-02 20:35:04 +0630

    Add Calc Description

--- src/com/ppm/Test.java
+++ src/com/ppm/Test.java
@@ -3,6 +3,7 @@ package com.ppm;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("Hello this is my Calc test.");
 		Calculator cal = new Calculator();
 		int addition = cal.add(5, 3);
 		int subtraction = cal.subtract(5, 3);
