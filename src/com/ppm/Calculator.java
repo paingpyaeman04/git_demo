e37e732122bdcd2dc493ec471ad2d0bdcf824429
Paing Pyae Man <paingpyaeman04@gmail.com>
2020-12-02 20:25:50 +0630

    Add divide and multiply methods

--- src/com/ppm/Calculator.java
+++ src/com/ppm/Calculator.java
@@ -9,5 +9,13 @@ public class Calculator {
 	public int subtract(int a, int b) {
 		return a - b;
 	}
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
 
 }
