b48890f57e15a47c3e92bf9a6375d296d537e617
Paing Pyae Man <62030108+paingpyaeman04@users.noreply.github.com>
2020-12-02 20:40:03 +0630

    Change to calculator version 2

--- src/com/ppm/Test.java
+++ src/com/ppm/Test.java
@@ -3,7 +3,7 @@ package com.ppm;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello this is my Calc test.");
+		System.out.println("Hello this is my Calculator Version2");
 		Calculator cal = new Calculator();
 		int addition = cal.add(5, 3);
 		int subtraction = cal.subtract(5, 3);
